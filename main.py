871f560104a7b8e6e9aa4616580fc2f925aa1475
Myth Shdw <mythshdw@email.com>
2021-06-24 13:07:44 +0600

    1aA

--- main.py
+++ main.py
@@ -4,9 +4,13 @@ rps_choice_list = ['Rock', 'Paper', 'Scissor']
 
 your_choice = input('Choice your RPS: ')
 
+your_choice = your_choice.lower()
+
 computer_random_choice = random.choice(rps_choice_list)
 
-if computer_random_choice == 'Rock' and your_choice == 'Rock':
+computer_random_choice = computer_random_choice.lower()
+
+if computer_random_choice == 'rock' and your_choice == 'rock':
     print('Draw')
 else:
     print('Computer choice is not same as yours')
